222614c26cc16574e4669c05b58fc5c1aa51b040
Oliver Norred <norredoliver@gmail.com>
2021-06-14 02:05:06 -0700

    fix it so it works with multiple elements!
    changed path `id` to "loop${index}"

--- legit-marquees.js
+++ legit-marquees.js
@@ -1,32 +1,48 @@
-document.querySelectorAll("svg.legit-marquee").forEach(el => {
-	const amount = (el.getAttribute("contentrepeat")) ? el.getAttribute("contentrepeat") : 1
+document.querySelectorAll("svg.legit-marquee").forEach((el, index) => {
+	let amount = (el.getAttribute("contentrepeat")) ? el.getAttribute("contentrepeat") : 1
 	console.log(amount)
-	const content = el.getAttribute("content").concat(" ").repeat(amount)
-	const size = (el.getAttribute("textsize")) ? el.getAttribute("textsize") : 24
+	let content = el.getAttribute("content").concat(" ").repeat(amount)
+	let size = (el.getAttribute("textsize")) ? el.getAttribute("textsize") : 24
 	
-	const pathElement =
-	`<path id="loop" fill="transparent" d="${el.getAttribute("pathd")}"></path>`
+	
+	let pathElement =
+	`<path id="loop${index}" fill="transparent" d="${el.getAttribute("pathd")}"></path>`
 
-	const textElement =
+	let textElement =
 	`<text width="100%">
-	 	<textPath alignment-baseline="top" xlink:href="#loop" startOffset="0" font-size="${size}" class="movingtextpath">${content}</textPath>
+		<textPath alignment-baseline="top" xlink:href="#loop${index}" startOffset="0" font-size="${size}" class="movingtextpath">${content}</textPath>
 	</text>`
 	el.innerHTML = `${pathElement}${textElement.repeat(2)}`
 
-
-	const path = el.querySelector("#loop")
-	const firsttext = el.querySelectorAll(".movingtextpath")[0]
-	const secondtext = el.querySelectorAll(".movingtextpath")[1]
 	
 	let offset = 0
-	const s = parseFloat(el.getAttribute("speed"));
-	const direction = s/Math.abs(s)
-	function updateTextOffset() {
+	let s = parseFloat(el.getAttribute("speed"))
+	let direction = s/Math.abs(s)
+	let path = el.querySelector(`#loop${index}`)
+	let firsttext, secondtext
+
+	firsttext = el.querySelectorAll(".movingtextpath")[0]
+	secondtext = el.querySelectorAll(".movingtextpath")[1]
+	
+
+	el.funcs = {}
+
+	el.funcs.updateTextOffset = function() {
 		offset += parseFloat(el.getAttribute("speed"))
 		if(offset > path.getTotalLength() || offset < -path.getTotalLength()) {offset = 0}
 		firsttext.setAttribute("startOffset", offset)
-		secondtext.setAttribute("startOffset", offset-direction*path.getTotalLength()+.5*direction)
-		requestAnimationFrame(updateTextOffset)
+		secondtext.setAttribute(
+			"startOffset",
+			offset - direction*path.getTotalLength() + 0.5*direction
+		)
 	}
-	updateTextOffset()
-});
+
+})
+
+function updatePage() {
+	document.querySelectorAll("svg.legit-marquee").forEach(el => {
+		el.funcs.updateTextOffset()
+	})
+	requestAnimationFrame(updatePage)
+}
+updatePage()
